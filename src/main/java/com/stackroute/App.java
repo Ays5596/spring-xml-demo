375712bea485a14822fd0b73533e2719211a4a85
Ays5596 <aysny5m96@gmail.com>
2019-10-03 10:13:43 +0530

    branch autowire

--- src/main/java/com/stackroute/App.java
+++ src/main/java/com/stackroute/App.java
@@ -10,14 +10,11 @@ public class App
 {
     public static void main( String[] args ) {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
-        Movie hr1 = applicationContext.getBean("movieA",Movie.class);
-        Movie hr2 = applicationContext.getBean("movieB",Movie.class);
+        Movie hr1 = (Movie)applicationContext.getBean("movie",Movie.class);
+        //Movie hr2 = applicationContext.getBean("movieB",Movie.class);
        // System.out.println(hr.getHR().getName());
        // System.out.println(hr.getHR().getGender());
        // System.out.println(hr.getHR().getAge());
-        hr2.disp1();
-        hr2.disp2();
-        hr2.disp3();
-        System.out.println(hr1==hr2);
+       hr1.disp1();
     }
 }
